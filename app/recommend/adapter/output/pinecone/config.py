1551f3c46b2dcdec5e6c8a1fe9c07c34b7762750
MinchoGreenT <dldmstjq99@gmail.com>
2024-05-12 05:45:30 +0900

    [fix] pinecone 앱 키 config 처리

--- app/recommend/adapter/output/pinecone/config.py
+++ app/recommend/adapter/output/pinecone/config.py
@@ -1,4 +1,6 @@
 from pinecone import Pinecone
 
-pc = Pinecone(api_key="bbfb4eb0-776f-4efc-ad13-b44bb07d8755")
+from core.config import config
+
+pc = Pinecone(api_key=config.PINECONE_API_KEY)
 index = pc.Index("user-item")
