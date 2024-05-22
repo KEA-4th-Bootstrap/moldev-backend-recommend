08bb6e9e514e566109307af339f57c926ae6bd01
MinchoGreenT <dldmstjq99@gmail.com>
2024-05-22 14:12:56 +0900

    [feat] 추천 서비스, 라우터

--- app/recommend/adapter/input/recommend.py
+++ app/recommend/adapter/input/recommend.py
@@ -4,22 +4,38 @@ from dependency_injector.wiring import Provide, inject
 from fastapi import APIRouter, Depends, Query, Path
 
 from app.container import Container
-from app.recommend.adapter.input.response import RecommendResponse
+from app.recommend.adapter.input.request import UserItemModifyRequest
 from app.recommend.application.service.recommend import RecommendService
+
+import logging
+
+# initialize logger
+logging.basicConfig(format='%(asctime)s - %(levelname)s - %(message)s',
+                    level=logging.INFO)
+log = logging.getLogger(__name__)
 
 recommend_router = APIRouter()
 
 
 @recommend_router.get(
-    "/{member_id}",
-    response_model=RecommendResponse
+    "/{member_id}"
 )
 @inject
 async def get_recommendation(
-        member_id: Annotated[str, Path(title="The ID of the item to get")],
+        member_id: Annotated[int, Path(title="The ID of the item to get")],
         recommend_service: RecommendService = Depends(Provide[Container.recommend_service])
 ):
-    return {"recommend_list": await recommend_service.get_recommend(member_id)}
+    return await recommend_service.get_recommend(member_id)
+
+
+@recommend_router.get(
+    "/all/item"
+)
+@inject
+async def get_all_items(
+        recommend_service: RecommendService = Depends(Provide[Container.recommend_service])
+):
+    return await recommend_service.get_all_items()
 
 
 @recommend_router.post(
@@ -31,3 +47,27 @@ async def update_vector(
 ):
     await recommend_service.update_data()
     return ""
+
+
+@recommend_router.patch(
+    ""
+)
+@inject
+async def update_user_item(
+        request: UserItemModifyRequest,
+        recommend_service: RecommendService = Depends(Provide[Container.recommend_service])
+):
+    await recommend_service.update_user_item(request.id, request.categories)
+    return ""
+
+
+@recommend_router.get(
+    "/post/content",
+)
+@inject
+async def get_post_categories(
+        post_content: Annotated[str, Query(title="The content of post to get categories")],
+        recommend_service: RecommendService = Depends(Provide[Container.recommend_service])
+):
+    log.info(f"post_content: {post_content}")
+    return await recommend_service.extract_categories(post_content)
--- app/recommend/application/service/recommend.py
+++ app/recommend/application/service/recommend.py
@@ -1,13 +1,99 @@
+from app.recommend.adapter.output.azureai.category_gpt import GPTAdapter
+from app.recommend.adapter.output.mongo.post_categories_repository import PostCategoriesRepositoryAdapter
+from app.recommend.adapter.output.mongo.user_item_repository import UserItmeRepositoryAdapter
 from app.recommend.adapter.output.pinecone.repository import VectorRepositoryAdapter
+
+import numpy as np
+import httpx
+
+import logging
+
+categories = [
+    "백엔드", "프론트엔드", "인공지능", "데브옵스", "인프라", "웹", "서버", "데이터베이스", "리눅스", "앱 네이티브",
+    "플러터", "자바스크립트", "어셈블리", "딥러닝", "머신러닝", "데이터과학", "대외활동", "동아리", "공모전", "해커톤",
+    "트러블슈팅", "네트워크", "운영체제", "컴퓨터구조", "알고리즘", "자료구조", "코딩테스트"
+]
+
+# initialize logger
+logging.basicConfig(format='%(asctime)s - %(levelname)s - %(message)s',
+                    level=logging.INFO)
+log = logging.getLogger(__name__)
+
+
+def normalize_values(all_user_items):
+    # 모든 values를 수집
+    all_values = np.array([item['values'] for item in all_user_items])
+
+    # 각 열의 최대값과 최소값 계산
+    max_values = np.max(all_values, axis=0) - 0.0001
+    min_values = np.min(all_values, axis=0) + 0.0001
+
+    # 정규화 함수
+    def normalize(value, min_value, max_value):
+        return float((value - min_value) / (max_value - min_value) if max_value != min_value else value)
+
+    # 모든 values 정규화
+    normalized_items = []
+    for item in all_user_items:
+        normalized_values = [
+            normalize(value, min_value, max_value)
+            for value, min_value, max_value in zip(item['values'], min_values, max_values)
+        ]
+        normalized_items.append({
+            "id": str(item["id"]),
+            "values": normalized_values
+        })
+
+    return normalized_items
 
 
 class RecommendService:
-    def __init__(self, vector_repo: VectorRepositoryAdapter):
+    def __init__(self, *,
+                 vector_repo: VectorRepositoryAdapter,
+                 user_item_repo: UserItmeRepositoryAdapter,
+                 post_categories_repo: PostCategoriesRepositoryAdapter,
+                 category_gpt: GPTAdapter):
         self.vector_repo = vector_repo
+        self.user_item_repo = user_item_repo
+        self.post_categories_repo = post_categories_repo
+        self.category_gpt = category_gpt
 
     async def update_data(self):
-        await self.vector_repo.upsert_data()
+        items = await self.get_all_items()
+        await self.vector_repo.upsert_data(items)
 
-    async def get_recommend(self, member_id: str):
+    async def get_recommend(self, member_id: int):
         result = await self.vector_repo.get_k_near(member_id)
-        return [result.get("matches")[i].get("id") for i in range(2)]
+        ids = [result.get("matches")[i].get("id") for i in range(2)]
+        url = "http://localhost:8080/api/member/recommend"
+        params = {
+            "memberIds": ids
+        }
+        async with httpx.AsyncClient() as client:
+            response = await client.get(url, params=params)
+            response.raise_for_status()  # 오류가 있는 경우 예외 발생
+            return response.json()
+
+    async def update_user_item(self, user_id: int, item_names: list):
+        await self.validate_user(user_id)
+        await self.user_item_repo.inc_items(user_id, item_names)
+
+    async def get_all_items(self):
+        items = self.user_item_repo.get_all_items()
+        log.info(items)
+        items = normalize_values(items)
+        return items
+
+    async def get_post_categories(self, post_id: int):
+        return self.post_categories_repo.get_post_categories(post_id)
+
+    async def extract_categories(self, post_content: str):
+        return await self.category_gpt.extract_category(post_content)
+
+    async def insert_post_categories(self, post_id: int, categories: list):
+        await self.post_categories_repo.insert_post_categories(post_id, categories)
+
+    async def validate_user(self, user_id: int):
+        user_item = await self.user_item_repo.get_user_item(user_id)
+        if not user_item:
+            await self.user_item_repo.insert_user_item(user_id, {category: 0 for category in categories})
