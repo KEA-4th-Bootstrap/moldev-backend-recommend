72939bd63b83f2c91f38726c00f1dd1a21035efc
MinchoGreenT <dldmstjq99@gmail.com>
2024-05-22 14:13:10 +0900

    [feat] 컨테이너 정의, 의존성 주입

--- app/container.py
+++ app/container.py
@@ -1,12 +1,22 @@
 from dependency_injector.containers import DeclarativeContainer, WiringConfiguration
 from dependency_injector.providers import Factory, Singleton
 
+from app.recommend.adapter.output.azureai.category_gpt import GPTAdapter
 from app.recommend.adapter.output.pinecone.repository import VectorRepositoryAdapter
 from app.recommend.application.service.recommend import RecommendService
+from app.recommend.adapter.output.mongo.user_item_repository import UserItmeRepositoryAdapter
+from app.recommend.adapter.output.mongo.post_categories_repository import PostCategoriesRepositoryAdapter
 
 
 class Container(DeclarativeContainer):
     wiring_config = WiringConfiguration(packages=["app"])
 
     vector_repository_adapter = Singleton(VectorRepositoryAdapter)
-    recommend_service = Singleton(RecommendService, vector_repo=vector_repository_adapter)
+    user_item_repository_adapter = Singleton(UserItmeRepositoryAdapter)
+    post_categories_repository_adapter = Singleton(PostCategoriesRepositoryAdapter)
+    category_gpt_adapter = Singleton(GPTAdapter)
+    recommend_service = Singleton(RecommendService,
+                                  vector_repo=vector_repository_adapter,
+                                  user_item_repo=user_item_repository_adapter,
+                                  post_categories_repo=post_categories_repository_adapter,
+                                  category_gpt=category_gpt_adapter)
--- app/recommend/container.py
+++ app/recommend/container.py
@@ -1,7 +1,10 @@
 from dependency_injector.containers import DeclarativeContainer, WiringConfiguration
 from dependency_injector.providers import Factory, Singleton
 
+from app.recommend.adapter.output.azureai.category_gpt import GPTAdapter
 from app.recommend.adapter.output.pinecone.repository import VectorRepositoryAdapter
+from app.recommend.adapter.output.mongo.user_item_repository import UserItmeRepositoryAdapter
+from app.recommend.adapter.output.mongo.post_categories_repository import PostCategoriesRepositoryAdapter
 from app.recommend.application.service.recommend import RecommendService
 
 
@@ -9,4 +12,11 @@ class RecommendContainer(DeclarativeContainer):
     wiring_config = WiringConfiguration(modules=["app"])
 
     vector_repository_adapter = Singleton(VectorRepositoryAdapter)
-    recommend_service = Singleton(RecommendService, vector_repo=vector_repository_adapter)
+    user_item_repository_adapter = Singleton(UserItmeRepositoryAdapter)
+    post_categories_repository_adapter = Singleton(PostCategoriesRepositoryAdapter)
+    category_gpt_adapter = Singleton(GPTAdapter)
+    recommend_service = Singleton(RecommendService,
+                                  vector_repo=vector_repository_adapter,
+                                  user_item_repo=user_item_repository_adapter,
+                                  post_categories_repo=post_categories_repository_adapter,
+                                  category_gpt=category_gpt_adapter)
