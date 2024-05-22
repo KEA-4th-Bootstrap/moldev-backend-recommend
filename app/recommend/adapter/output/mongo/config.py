a712add3b247ee329605e4a30e4e308c6acee5f4
MinchoGreenT <dldmstjq99@gmail.com>
2024-05-22 14:13:45 +0900

    [fix] 몽고db 컨피그

---
+++ app/recommend/adapter/output/mongo/config.py
@@ -0,0 +1,16 @@
+from pymongo.mongo_client import MongoClient
+from pymongo.server_api import ServerApi
+
+from core.config import config
+
+client = MongoClient(config.MONGO_DB_URL, server_api=ServerApi("1"))
+
+try:
+    client.admin.command("ping")
+    print("Pinged your deployment. You successfully connected to MongoDB!")
+except Exception as e:
+    print(e)
+
+db = client.recommend
+user_item = db["user_item"]
+post_categories = db["post_categories"]
